b55334799ae0fefd6d848854b43ec23aab0447d8
Zac McCormick <zac.mccormick@gmail.com>
2021-06-02 22:28:21 -0400

    Add yearn API

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,26 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+// import useSWR from 'swr'
+// import Person from '../components/Person'
+
+// const fetcher = (url) => fetch(url).then((res) => res.json())
+
+// export default function Index() {
+//   const { data, error } = useSWR('/api/people', fetcher)
+
+//   if (error) return <div>Failed to load</div>
+//   if (!data) return <div>Loading...</div>
+
+//   return (
+//     <ul>
+//       {data.map((p, i) => (
+//         <Person key={i} person={p} />
+//       ))}
+//     </ul>
+//   )
+// }
+
 export default function Home() {
   return (
     <div className={styles.container}>
